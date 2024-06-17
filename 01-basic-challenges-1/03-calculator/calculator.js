645175f67d3ce37852c5d530c6d62c7dace6f4ea
ebenofere <eofere@gmail.com>
2024-06-17 12:10:20 -0400

    update

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,27 @@
-function calculator() {}
+function calculator(a, b, operator) {
+    // declare a variable to store the result
+    let result;
+
+    // use a switch statement to determine which operation to perform
+    switch(operator) {
+        case "+":
+            result = a + b;
+            break;
+        case "-":
+            result = a - b;
+            break; 
+        case "*":
+            result = a * b;
+            break;
+        case "/":
+            result = a / b;
+            break;
+        default:
+            // if the operator is not one of the above, throw an error
+            throw new Error("Invalid operator");
+    }
+
+    return result;
+}
 
 module.exports = calculator;
