cb6615e72add1d89371b788a89987d3e914472b4
ebenofere <eofere@gmail.com>
2024-06-18 10:44:57 -0400

    update

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,14 @@
-function countOccurrences() {}
+function countOccurrences(word, character) {
+    let newArray = word.split("");
+    let sum = 0;
+
+   	newArray.forEach((item) => {
+      if (item == character) {
+        sum++;
+      }
+   	});
+    
+    return sum;
+}
 
 module.exports = countOccurrences;
