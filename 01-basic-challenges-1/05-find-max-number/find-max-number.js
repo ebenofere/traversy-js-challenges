93626ea70c43e4f093732f6d7f963a28c68cda43
ebenofere <eofere@gmail.com>
2024-06-18 23:18:56 -0400

    update

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,16 +1,13 @@
 function findMaxNumber(arr) {
     let max = arr[0];
 
-    for (let i = 1; i < arr.length; i++) {
+    for (i = 1; i < arr.length; i++) {
         if (arr[i] > max) {
             max = arr[i];
         }
     }
 
     return max;
-
-
-    
 
 }
 
