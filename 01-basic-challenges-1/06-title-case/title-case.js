e0746b112b93d63332e0ae21a6838e1421018671
ebenofere <eofere@gmail.com>
2024-06-20 00:02:07 -0400

    update

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,17 +1,18 @@
-function titleCase(arr) {
-    let newArray = arr.toLowerCase().split(" ");
-    newArray.forEach((item) => {
-        let newItem = item.charAt(0).toUpperCase() + item.slice(1);
-        
-        let newArray = newItem.push(36)
+// Solution 1 -using a for loop
+function titleCase(str) {
+    // make the sentence to be in lowercase and break the sentence into an array of words
+    const words = str.toLowerCase().split(' ');
 
-        // console.log(newArray2);
+    //use a for loop to iterate through each item in the array
+    for (i = 0; i < words.length; i++) {
+        // make the first word in each item in the array to be in capital letter while the rest of the characters in the item is lowercase.
+        words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+    }
 
-
-        
-    })
-    
+    // join the items and then return it
+    return words.join(" ");
 }
+
 
 // charAt(0).toUpperCase()
 module.exports = titleCase;
