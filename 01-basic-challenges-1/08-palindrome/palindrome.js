0b46465677f1190aa8f58632e6bcbb1bb5252cca
ebenofere <eofere@gmail.com>
2024-07-07 07:33:14 -0400

    update

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,9 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+	let reverseStr = str.split('').reverse().join('');
+  
+    return str == reverseStr;
+  
+}
+
 
 module.exports = isPalindrome;
